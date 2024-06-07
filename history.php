311ad8a667a2d72975b09459041262a4d16da613
JeremyMachadoHerrera <115716348+JeremyMachadoHerrera@users.noreply.github.com>
2024-06-07 23:08:11 +0200

    Create history.php

---
+++ history.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Historia - Keymasters</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="logo.png" alt="Logo de Keymasters" >
+            <h1>Keymasters</h1>
+        </div>
+        <nav>
+            <a href="index.php">Inicio</a>
+            <div class="dropdown">
+                <a href="about.php" class="dropbtn">Sobre Nosotros</a>
+                <div class="dropdown-content">
+                    <a href="history.php">Nuestra Historia</a>
+                    <a href="mission.php">Nuestra Misión</a>
+                    <a href="team.php">Nuestro Equipo</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <a href="services.php" class="dropbtn">Servicios</a>
+                <div class="dropdown-content">
+                    <a href="consulting.php">Consultoría</a>
+                    <a href="development.php">Desarrollo</a>
+                    <a href="support.php">Soporte</a>
+                </div>
+            </div>
+            <a href="contact.php">Contacto</a>
+            <a href="blog.php">Blog</a>
+        </nav>
+    </header>
+    <div class="container">
+        <section class="history-section">
+            <h2>Nuestra Historia</h2>
+            <p>Keymasters fue fundada en el año 2024 por Agel de la hoz y Jeremy Machado Herrera con la visión de proporcionar soluciones tecnológicas innovadoras a empresas de todos los tamaños. Desde entonces, hemos experimentado un crecimiento constante y nos hemos consolidado como líderes en la industria de la consultoría informática.</p>
+            <h3>Sus Orígenes</h3>
+            <p>Detalles sobre cómo y por qué se fundó la empresa, los objetivos iniciales y los primeros pasos.</p>
+            <h3>Hitos Importantes</h3>
+            <ul>
+                <li>Año 08/03/2024: idea de Creacion de la empresa</li>
+                <li>Año XXXX: [Descripción del hito importante]</li>
+                
+            </ul>
+            <h3>Nuestro Legado</h3>
+            <p>La dedicación a la excelencia, la innovación constante y el compromiso con nuestros clientes son los pilares de nuestro legado. Seguimos trabajando con pasión y determinación para alcanzar nuevos logros y dejar una marca positiva en la industria.</p>
+        </section>
+    </div>
+    <footer>
+        <p>&copy; 2024 Keymasters. Todos los derechos reservados.</p>
+    </footer>
+</body>
+</html>
